91157ef10f657f16a0f20064d04fe681e021e324
GGhiaroni <gghiaronitiziano@gmail.com>
2026-04-20 23:44:12 -0300

    feat: Adiciona os endpoints de listar todas as notificações do cliente logado e notificação por id + seus respectivos métodos no service

--- src/main/java/com/GabrielTiziano/Notify/controller/NotificationController.java
+++ src/main/java/com/GabrielTiziano/Notify/controller/NotificationController.java
@@ -6,10 +6,9 @@ import com.GabrielTiziano.Notify.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/notify/notifications")
@@ -20,5 +19,15 @@ public class NotificationController {
     @PostMapping("/send")
     public ResponseEntity<NotificationResponseDTO> sendNotification(@RequestBody NotificationRequestDTO notificationRequestDTO){
         return ResponseEntity.status(HttpStatus.CREATED).body(notificationService.sendNotification(notificationRequestDTO));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<NotificationResponseDTO> getNotificationById(@PathVariable String id){
+        return ResponseEntity.ok(notificationService.getNotificationById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<NotificationResponseDTO>> getMyNotifications() {
+        return ResponseEntity.ok(notificationService.getMyNotifications());
     }
 }
--- src/main/java/com/GabrielTiziano/Notify/repository/NotificationRepository.java
+++ src/main/java/com/GabrielTiziano/Notify/repository/NotificationRepository.java
@@ -3,5 +3,8 @@ package com.GabrielTiziano.Notify.repository;
 import com.GabrielTiziano.Notify.model.NotificationModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface NotificationRepository extends MongoRepository<NotificationModel, String> {
+    List<NotificationModel> findAllByClientId(String clientId);
 }
