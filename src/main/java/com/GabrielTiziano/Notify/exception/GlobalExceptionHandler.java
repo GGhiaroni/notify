5d04c7d5817d23b101358cff7d3ec2cc3d1dc5ea
GGhiaroni <gghiaronitiziano@gmail.com>
2026-04-20 23:47:34 -0300

    feat: Cria as exceptions de user já cadastrado, usuário não existe e Business Rule Exception

---
+++ src/main/java/com/GabrielTiziano/Notify/exception/GlobalExceptionHandler.java
@@ -0,0 +1,58 @@
+package com.GabrielTiziano.Notify.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.Instant;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request){
+        HttpStatus status = HttpStatus.NOT_FOUND;
+
+        StandardError error = new StandardError(
+                Instant.now(),
+                status.value(),
+                "Resource not found",
+                e.getMessage(),
+                request.getRequestURI()
+        );
+
+        return ResponseEntity.status(status).body(error);
+    }
+
+    @ExceptionHandler(BusinessRuleException.class)
+    public ResponseEntity<StandardError> businessRule(BusinessRuleException e, HttpServletRequest request){
+        HttpStatus status = HttpStatus.CONFLICT;
+
+        StandardError error = new StandardError(
+          Instant.now(),
+                status.value(),
+                "Business Rule Violation",
+        e.getMessage(),
+        request.getRequestURI()
+        );
+
+        return ResponseEntity.status(status).body(error);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<StandardError> globalException(Exception e, HttpServletRequest request){
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+
+        StandardError error = new StandardError(
+                Instant.now(),
+                status.value(),
+                "Internal server error",
+                "Ocorreu um erro inesperado no servidor. Contate o suporte.",
+                request.getRequestURI()
+        );
+
+        return ResponseEntity.status(status).body(error);
+    }
+
+}
---
+++ src/main/java/com/GabrielTiziano/Notify/exception/StandardError.java
@@ -0,0 +1,12 @@
+package com.GabrielTiziano.Notify.exception;
+
+import java.time.Instant;
+
+public record StandardError(
+        Instant timestamp,
+        Integer status,
+        String error,
+        String message,
+        String path
+) {
+}
